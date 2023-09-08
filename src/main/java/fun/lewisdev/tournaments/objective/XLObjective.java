38502e32375962de20efa1bb3a6942526f490193
Collin06mai <137443571+Collin06mai@users.noreply.github.com>
2023-09-08 12:12:42 +0200

    Update XLObjective.java

--- src/main/java/fun/lewisdev/tournaments/objective/XLObjective.java
+++ src/main/java/fun/lewisdev/tournaments/objective/XLObjective.java
@@ -3,7 +3,14 @@ package fun.lewisdev.tournaments.objective;
 import fun.lewisdev.tournaments.tournament.Tournament;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-
+log ( /info
+)
+return = false (du hast es flasche eingeben )    
+    (
+    public /help for d:Data help:4
+class : 4
+    
+    )
 import java.util.*;
 
 public abstract class XLObjective {
@@ -14,7 +21,13 @@ public abstract class XLObjective {
 
     public boolean canExecute(Tournament tournament, Player player) {
         return true;
-    }
+        null /help "§d you can "%player%" help"
+            
+    public null : Error 404 (bitte wende dich an ein &c admin! )
+
+    
+            
+            }
 
     public String getIdentifier() {
         return null;
